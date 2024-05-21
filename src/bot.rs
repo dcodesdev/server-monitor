53ab32f6edb42fb4be77318b1f2b0adf729efaee
dcodesdev <101001810+dcodesdev@users.noreply.github.com>
2024-05-21 11:56:21 +0300

    better bot.rs code

--- src/bot.rs
+++ src/bot.rs
@@ -1,22 +1,19 @@
 use teloxide::prelude::*;
 
 pub fn create_bot() -> Bot {
-    let bot: Bot = Bot::from_env();
-
-    bot
+    Bot::from_env()
 }
 
 pub async fn notify<'a>(opts: &NotifyOpts<'a>) -> anyhow::Result<()> {
     let telegram_chat_id = std::env::var("TELEGRAM_CHAT_ID").expect("TELEGRAM_CHAT_ID must be set");
     let result = opts.bot.send_message(telegram_chat_id, &opts.message).await;
 
-    match result {
-        Ok(_) => Ok(()),
-        Err(e) => {
-            eprintln!("Failed to send message to Telegram: {}", e);
-            Ok(())
-        }
+    if let Err(e) = result {
+        eprintln!("Failed to send message to Telegram: {}", e);
+        return Err(e.into());
     }
+
+    Ok(())
 }
 
 #[derive(Debug)]
