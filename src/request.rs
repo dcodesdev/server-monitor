3e2b3c0183765f71043759c566aa6a4fa3bfad3c
dcodesdev <101001810+dcodesdev@users.noreply.github.com>
2024-05-28 16:39:07 +0300

    no status code bug fix

--- src/request.rs
+++ src/request.rs
@@ -1,7 +1,11 @@
 /// Returns `true` if the URL is up
 /// Returns `false` if down
 pub async fn url_lookup(url: &str) -> anyhow::Result<bool> {
-    let response = reqwest::get(url).await?;
+    let response = match reqwest::get(url).await {
+        Err(_) => return Ok(false),
+        Ok(res) => res,
+    };
+
     let status = response.status();
 
     let status = if status.is_success() || status == 429 {
