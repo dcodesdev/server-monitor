8a333a38de5c534c0d9b0252ea9e979332030d7a
dcodesdev <101001810+dcodesdev@users.noreply.github.com>
2024-05-28 17:50:21 +0300

    bug fix

--- src/request.rs
+++ src/request.rs
@@ -1,30 +1,23 @@
+use reqwest::StatusCode;
+use std::time::Duration;
+
 /// Returns `true` if the URL is up
 /// Returns `false` if down
 pub async fn url_lookup(url: &str) -> anyhow::Result<bool> {
-    let response = match reqwest::get(url).await {
-        Err(e) => {
-            let status = e.status();
+    let client = reqwest::Client::builder()
+        .timeout(Duration::from_secs(5))
+        .build()?;
 
-            if let Some(status) = status {
-                if status == 429 {
-                    return Ok(true);
-                }
+    match client.get(url).send().await {
+        Err(_) => return Ok(false),
+        Ok(res) => {
+            let status = res.status();
 
-                return Ok(false);
-            } else {
-                return Ok(false);
-            };
+            if status.is_success() || status == StatusCode::TOO_MANY_REQUESTS {
+                return Ok(true);
+            }
+
+            return Ok(false);
         }
-        Ok(res) => res,
-    };
-
-    let status = response.status();
-
-    let status = if status.is_success() || status == 429 {
-        true
-    } else {
-        false
-    };
-
-    Ok(status)
+    }
 }
