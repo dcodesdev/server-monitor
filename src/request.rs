cb96a5df1d13e9fd99d60403af58c601a3605e3c
dcodesdev <101001810+dcodesdev@users.noreply.github.com>
2024-05-28 16:51:17 +0300

    request bug fix

--- src/request.rs
+++ src/request.rs
@@ -2,7 +2,19 @@
 /// Returns `false` if down
 pub async fn url_lookup(url: &str) -> anyhow::Result<bool> {
     let response = match reqwest::get(url).await {
-        Err(_) => return Ok(false),
+        Err(e) => {
+            let status = e.status();
+
+            if let Some(status) = status {
+                if status == 429 {
+                    return Ok(true);
+                }
+
+                return Ok(false);
+            } else {
+                return Ok(false);
+            };
+        }
         Ok(res) => res,
     };
 
